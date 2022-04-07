997f14ccd6a7e1fd219ff8c8c26967fbf1150702
Filipp Shkliaev <fishkliaev@gmail.com>
2022-04-07 16:28:21 +0300

    Revert changing of lobby scene

--- vr-duel/Assets/CodeBase/StaticData/AssetsPath.cs
+++ vr-duel/Assets/CodeBase/StaticData/AssetsPath.cs
@@ -16,7 +16,7 @@
         public const string ConfigHolder = "Network/Network Config Holder";
         public const string InitialSceneName = "Loader";
         public const string NameSelectionSceneName = "NameSelection";
-        public const string LobbySceneName = "Game";
+        public const string LobbySceneName = "Lobby";
         public const string GameSceneName = "Game";
     }
 }
